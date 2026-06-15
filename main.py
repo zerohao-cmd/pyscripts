c1d638aab1f4c7fbf14a85c6e8359916010f9a62
zero <sda4444@qq.com>
2026-06-15 21:13:35 +0800

    FEAT: add mind

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from pysripts!")
+from datetime import datetime, timezone, timedelta, date
+from zoneinfo import ZoneInfo
 
 
 if __name__ == "__main__":
-    main()
+    # tz = timezone(timedelta(hours=8))
+    tz = ZoneInfo("Asia/Shanghai")
+    dt1 = datetime(2025, 6, 15, 12, 0, tzinfo=tz)
+    dt2 = datetime(2025, 6, 15, 12, 0)
+    dt3 = date(2025, 6, 15)
+    print(dt1.isoformat())
+    print(dt2.isoformat())
+    print(dt3.isoformat())
